e85ae248cbf785a37f12ed57a2342956bd47d2ec
ElderJames <shunjiey@hotmail.com>
2026-09-25 21:18:19 +0800

    Read legacy icon names from React transform

--- src/AntDesign.Icons.Generator/IconGeneratorHelper.cs
+++ src/AntDesign.Icons.Generator/IconGeneratorHelper.cs
@@ -10,16 +10,6 @@ namespace AntDesign.Icons.Generator
 {
     public class IconGeneratorHelper
     {
-        // Mirrors @ant-design/icons-svg 4.6.0's adjustViewBox transform.
-        // React retains the original canvas only for these legacy icon names.
-        private static readonly HashSet<string> LegacyV6ViewBoxNames = new(StringComparer.Ordinal)
-        {
-            "step-backward", "step-forward", "fast-backward", "fast-forward", "forward", "backward",
-            "caret-up", "caret-down", "caret-left", "caret-right", "retweet", "swap-left", "swap-right",
-            "loading", "loading-3-quarters", "coffee", "bars", "file-jpg", "inbox", "shopping-cart",
-            "safety", "medium-workmark"
-        };
-
         public static string IconTemplate = """
             using Microsoft.AspNetCore.Components;
             using Microsoft.AspNetCore.Components.Rendering;
@@ -85,7 +75,7 @@ namespace AntDesign.Icons.Generator
             """;
         }
 
-        public static string GetIconClassTemplate(string iconName, string className, string content)
+        public static string GetIconClassTemplate(string iconName, string className, string content, HashSet<string> legacyViewBoxNames)
         {
             var template = IconTemplate;
             var towtone = className.EndsWith("Twotone");
@@ -136,7 +126,7 @@ namespace AntDesign.Icons.Generator
             xml.LoadXml(content);
 
             var attributes = xml.DocumentElement.Attributes.Cast<XmlAttribute>().ToDictionary(static attr => attr.Name, static attr => attr.Value);
-            attributes["viewBox"] = LegacyV6ViewBoxNames.Contains(iconName)
+            attributes["viewBox"] = legacyViewBoxNames.Contains(iconName)
                 ? "0 0 1024 1024"
                 : "64 64 896 896";
 
